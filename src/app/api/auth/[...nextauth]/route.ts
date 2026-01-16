419494ee0eff1ef3c943501299f1081e1d32fda4
senpaioka@linux <abdulmannan7589@gmail.com>
2026-01-17 00:42:15 +0600

    minor fixes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,25 +1,2 @@
-// import { authOptions } from "@/auth";
-// import NextAuth from "next-auth";
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
-
-
-// import NextAuth from "next-auth"
-// import { authOptions } from "@/auth"
-// import { NextRequest, NextResponse } from "next/server"
-
-// export async function GET(req: NextRequest) {
-//   const res = await NextAuth(authOptions)
-//   // Convert the NextAuth result into a NextResponse
-//   return new NextResponse(res.body, { status: res.status, headers: res.headers })
-// }
-
-// export async function POST(req: NextRequest) {
-//   const res = await NextAuth(authOptions)
-//   return new NextResponse(res.body, { status: res.status, headers: res.headers })
-// }
-
-
 import { handlers } from "@/auth" // Referring to the auth.ts
 export const { GET, POST } = handlers
--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,8 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 import Credentials from "next-auth/providers/credentials"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
 
     Credentials({
       name: "Credentials",
@@ -89,71 +84,4 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
   
 })
-
-
-// import Google from "next-auth/providers/google"
-// import Credentials from "next-auth/providers/credentials"
-
-// export const authOptions = {
-//   providers: [
-//     Google({
-//       clientId: process.env.GOOGLE_ID!,
-//       clientSecret: process.env.GOOGLE_SECRET!,
-//     }),
-//     Credentials({
-//       name: "Credentials",
-//       credentials: {
-//         email: { label: "Email", type: "email" },
-//         password: { label: "Password", type: "password" },
-//       },
-
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials.password) return null
-
-//         if (credentials.email === "staff@mail.com" && credentials.password === "1234") {
-//           return { id: "1000", 
-//             name: "Staff", email: "staff@mail.com", 
-//             role: "staff" }
-//         }
-//         if (credentials.email === "admin@mail.com" && credentials.password === "1234") {
-//           return { id: "1101", 
-//             name: "Admin", email: "admin@mail.com", 
-//             role: "admin" }
-//         }
-//         if (credentials.email === "customer@mail.com" && credentials.password === "1234") {
-//           return { id: "1102", 
-//             name: "Customer", email: "customer@mail.com", 
-//             role: "customer" }
-//         }
-
-//         return null
-//       },
-//     }),
-//   ],
-
-//   callbacks: {
-//     async session({ session, token }) {
-//       const allowedRoles = ["admin", "staff", "customer"] as const
-
-//       if (token.role && allowedRoles.includes(token.role as any)) {
-//         session.user.role = token.role as typeof allowedRoles[number]
-//       }
-
-//       return session
-//     },
-
-//     async jwt({ token, user }) {
-//       if (user && (user as any).role) {
-//         token.role = (user as any).role as "admin" | "staff" | "customer"
-//       }
-//       return token
-//     },
-//   },
-
-//   pages: {
-//     signIn: "/login",
-//   },
-
-//   secret: process.env.NEXT_AUTH_SECRET,
-// }
 
